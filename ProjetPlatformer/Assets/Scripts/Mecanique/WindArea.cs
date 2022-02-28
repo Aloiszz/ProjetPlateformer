699c5ba754c08c8d2e2612cacde9033b62542431
Aloiszz <a.tupinon@rubika-edu.com>
2022-02-28 22:06:11 +0100

    Début de la méca du vent et fix du jump

---
+++ ProjetPlatformer/Assets/Scripts/Mecanique/WindArea.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindArea : MonoBehaviour
+{
+    public bool isWindy = false;
+    public float WindForce = 45f;
+    public float timeWaitForWind = 3f;
+    public CharacterMovement Character;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = Character.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        StartCoroutine(WaitForWind());
+        isWindy = true;
+    } 
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isWindy = false;
+    }
+
+    IEnumerator WaitForWind()
+    {
+        yield return new WaitForSeconds(timeWaitForWind);
+        rb.AddForce(new Vector2(WindForce, 0));
+    }
+}
