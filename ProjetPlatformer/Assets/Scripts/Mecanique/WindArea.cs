f38a4c2b4cfe6ed8dc67676340fd1e39b23c9dcb
Aloiszz <a.tupinon@rubika-edu.com>
2022-03-02 15:28:05 +0100

    Méca du vent fonctionnel mais tjr des bugs
    bugs si le joueur continue d'avencer pendant que la tempete est a son paroxisme

--- ProjetPlatformer/Assets/Scripts/Mecanique/WindArea.cs
+++ ProjetPlatformer/Assets/Scripts/Mecanique/WindArea.cs
@@ -7,6 +7,7 @@ public class WindArea : MonoBehaviour
 {
     public bool isWindy = false;
     public float WindForce = 45f;
+    public float WindForceNull = 0f;
     public float timeWaitForWind = 3f;
     public CharacterMovement Character;
     private Rigidbody2D rb;
@@ -15,13 +16,24 @@ public class WindArea : MonoBehaviour
     {
         rb = Character.GetComponent<Rigidbody2D>();
     }
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isWindy = true;
+    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        rb.AddForce(new Vector2(WindForce, 0));
-        //StartCoroutine(WaitForWind());
-    } 
+        if (isWindy)
+        {
+            rb.AddForce(new Vector2(WindForceNull, 0));
+        }
+        else
+        {
+            rb.AddForce(new Vector2(WindForce, 0));
+        }
+        StartCoroutine(WaitForWind());
+    }
 
     private void OnTriggerExit2D(Collider2D other)
     {
@@ -30,7 +42,12 @@ public class WindArea : MonoBehaviour
 
     IEnumerator WaitForWind()
     {
-        yield return new WaitForSeconds(timeWaitForWind);
-        isWindy = true;
+        while (isWindy)
+        {
+            yield return new WaitForSeconds(timeWaitForWind);
+            isWindy = false;
+            yield return new WaitForSeconds(timeWaitForWind);
+            isWindy = true;
+        }
     }
 }
