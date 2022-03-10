8978dfdc686250c3930bce1f6d384df2f84f578b
Aloiszz <a.tupinon@rubika-edu.com>
2022-03-10 01:38:13 +0100

    Travail sur LD1
    rework de la méca de boite, camera, perso

--- ProjetPlatformer/Assets/Scripts/Mecanique/PlaqueDePression.cs
+++ ProjetPlatformer/Assets/Scripts/Mecanique/PlaqueDePression.cs
@@ -23,7 +23,7 @@ public class PlaqueDePression : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Respawn")
         {
             OuverturePorte(); 
         }
