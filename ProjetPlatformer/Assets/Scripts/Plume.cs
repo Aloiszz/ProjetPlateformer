d7cdb6f58c66846b2a70da2be79dda776be455f4
Aloiszz <a.tupinon@rubika-edu.com>
2022-03-12 21:22:05 +0100

    Ajout de la plume et jump buffer

---
+++ ProjetPlatformer/Assets/Scripts/Plume.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.Sprites;
+using UnityEngine;
+
+public class Plume : MonoBehaviour
+{
+    public float timeToRespawn = 3f;
+    private SpriteRenderer renderer;
+    private Collider2D coll;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        coll = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CharacterMovement.instance.extrajumps += 1;
+            StartCoroutine(TimeToRespawn());
+        }
+    }
+
+    IEnumerator TimeToRespawn()
+    {
+        renderer.enabled = false;
+        coll.enabled = false;
+        yield return new WaitForSeconds(timeToRespawn);
+        renderer.enabled = true;
+        coll.enabled = true;
+    }
+}
