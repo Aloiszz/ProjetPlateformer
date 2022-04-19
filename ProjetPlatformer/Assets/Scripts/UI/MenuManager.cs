49e645cdfc8e34d0ff936afb1d31427a7db1b428
Ethan Dehay <e.dehay@rubika-edu.com>
2022-04-20 01:23:54 +0200

    Menu Parchemin fontionnel + Parallax mieux ?

--- ProjetPlatformer/Assets/Scripts/UI/MenuManager.cs
+++ ProjetPlatformer/Assets/Scripts/UI/MenuManager.cs
@@ -9,28 +9,38 @@ using XInputDotNetPure;
 
 public class MenuManager : MonoBehaviour/*, IPointerClickHandler*/
 {
-    
+    [Header("Bool Open/Close")]
     public bool MenuParcheminOuvert;
     public bool MenuPrincipalOuvert;
+    public bool IsChanging;
+    public int pageOuverte = 1;
 
-    
+    [Header("Divers")]
     public FeuxDeCamp Fdc;
     [SerializeField] private CanvasGroup cv;
     private Tween fadeTween;
     public Animator parchAnim;
 
-    
+    [Header("First Selected")]
     public GameObject firstSelctedOption;
     public GameObject firstSelctedMain;
     public GameObject firstSelectedPause;
     public GameObject firstSelectedParchemmin;
+    public GameObject fleche1;
+    public GameObject fleche2;
+    public GameObject fleche3;
+    public GameObject fleche4;
     
+    [Header("Éléments du Menu")]
     public GameObject parcheminManager;
     public GameObject mainMenu;
     public GameObject menuPrincipal;
     public GameObject menuOption;
     public GameObject menuPause;
     public GameObject menuParchemin;
+    public GameObject Page1;
+    public GameObject Page2;
+    public GameObject Page3;
     
     
     public static MenuManager instance;
@@ -98,16 +108,15 @@ public class MenuManager : MonoBehaviour/*, IPointerClickHandler*/
 
         //if (Fdc.onoff)
         //{
-            if (Input.GetAxis("BouttonMenuParchemin") > 0.1f)
+            if (Input.GetAxis("BouttonMenuParchemin") > 0.1f || Input.GetKeyDown(KeyCode.E))
             {
-                
                 OpenMenuParchemin();
             }
         //}
 
         if (MenuParcheminOuvert)
         {
-            if (Input.GetKeyDown(KeyCode.JoystickButton1))
+            if (Input.GetKeyDown(KeyCode.JoystickButton1) || Input.GetKeyDown(KeyCode.B))
             {
                 CloseMenuParchemin();
             }
@@ -119,19 +128,105 @@ public class MenuManager : MonoBehaviour/*, IPointerClickHandler*/
     {
         parchAnim.SetBool("FadeInParch",false);
         parchAnim.SetBool("FadeOutParch",true);
-        
-        
         MenuParcheminOuvert = true;
         menuParchemin.SetActive(true);
-        EventSystem.current.SetSelectedGameObject(null);
-        EventSystem.current.SetSelectedGameObject(firstSelectedParchemmin);
-                
-        CharacterMovement.instance.canMove = false;
-        CharacterMovement.instance.canJump = false;
-        CharacterMovement.instance.speed = 0;
+        if (pageOuverte == 1)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche1);
+        }
+        
+        if (pageOuverte == 2)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche2);
+        }
+        
+        if (pageOuverte == 3)
+        {
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche4);
+        }
+        
     }
 
+    public void ChangementPageDroite()
+    {
+        if (!IsChanging)
+        {
+            StartCoroutine(Changement());
+            float newPosPage1 = Page1.transform.position.x - 870;
+            float newPosPage2 = Page2.transform.position.x - 870;
+            float newPosPage3 = Page3.transform.position.x - 870;
+            Page1.transform.DOMove(new Vector3(newPosPage1,Page1.transform.position.y,Page1.transform.position.z), 1.5f);
+            Page2.transform.DOMove(new Vector3(newPosPage2,Page2.transform.position.y,Page2.transform.position.z), 1.5f);
+            Page3.transform.DOMove(new Vector3(newPosPage3,Page3.transform.position.y,Page3.transform.position.z), 1.5f).OnComplete((() => IsChanging = false));
+        }
+        
+    }
 
+    IEnumerator Changement()
+    {
+        yield return new WaitForSeconds(0.1f);
+        IsChanging = true;
+    }
+    
+    public void ChangementPageGauche()
+    {
+        if (!IsChanging)
+        {
+            StartCoroutine(Changement());
+            float newPosPage1 = Page1.transform.position.x + 870;
+            float newPosPage2 = Page2.transform.position.x + 870;
+            float newPosPage3 = Page3.transform.position.x + 870;
+            Page1.transform.DOMove(new Vector3(newPosPage1,Page1.transform.position.y,Page1.transform.position.z), 1.5f);
+            Page2.transform.DOMove(new Vector3(newPosPage2,Page2.transform.position.y,Page2.transform.position.z), 1.5f);
+            Page3.transform.DOMove(new Vector3(newPosPage3,Page3.transform.position.y,Page3.transform.position.z), 1.5f).OnComplete((() => IsChanging = false));
+        }
+    }
+
+    public void ChangementSelected1()
+    {
+        if (!IsChanging)
+        {
+            pageOuverte = 2;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche3);
+        }
+        
+    }
+    
+    public void ChangementSelected2()
+    {
+        if (!IsChanging)
+        {
+            pageOuverte = 1;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche1);
+        }
+    }
+    
+    public void ChangementSelected3()
+    {
+        if (!IsChanging)
+        {
+            pageOuverte = 3;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche4);
+        }
+    }
+    
+    public void ChangementSelected4()
+    {
+        if (!IsChanging)
+        {
+            pageOuverte = 2;
+            EventSystem.current.SetSelectedGameObject(null);
+            EventSystem.current.SetSelectedGameObject(fleche2);
+        }
+    }
+    
+    
     public void CloseMenuParchemin()
     {
         parchAnim.SetBool("FadeInParch",true);
@@ -139,10 +234,6 @@ public class MenuManager : MonoBehaviour/*, IPointerClickHandler*/
         
         MenuParcheminOuvert = false;
         menuParchemin.SetActive(false);
-        
-        CharacterMovement.instance.canMove = true;
-        CharacterMovement.instance.canJump = true;
-        CharacterMovement.instance.speed = 11;
     }
     
     
