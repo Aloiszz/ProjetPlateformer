8e67e5d9d79bd63e032631835c9f69c9e38be298
Aloiszz <a.tupinon@rubika-edu.com>
2022-05-28 15:22:39 +0200

    ahahhhh hh tempete

--- ProjetPlatformer/Assets/Scripts/EffetVent.cs
+++ ProjetPlatformer/Assets/Scripts/EffetVent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
@@ -9,12 +10,42 @@ public class EffetVent : MonoBehaviour
     public float xPos;
     public float yPos;
 
-    // Update is called once per frame
-    void Update()
+    public Animator animTempete;
+
+    public static EffetVent instance;
+    public WindArea windArea;
+
+    private void Awake()
     {
-        StartCoroutine(AttenteVent());
-        transform.position = camera.transform.position - new Vector3(xPos, yPos, camera.transform.position.z);
+        if (instance == null) instance = this;
     }
+
+    private void Start()
+    {
+        animTempete.SetBool("CanBegin", true);
+        animTempete.SetBool("CanEnd", false);
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //StartCoroutine(AttenteVent());
+        transform.position = camera.transform.position - new Vector3(xPos, yPos, camera.transform.position.z);
+        
+
+        if (windArea.isWindy == false)
+        {
+            //GetComponent<Animator>().Play("AnimationVent",5);
+            animTempete.SetBool("CanBegin", true);
+            animTempete.SetBool("CanEnd", false);
+        }
+        else 
+        {
+            animTempete.SetBool("CanEnd", true);
+            animTempete.SetBool("CanBegin", false);
+        }
+    }
+    
 
     IEnumerator AttenteVent()
     {
@@ -23,7 +54,7 @@ public class EffetVent : MonoBehaviour
             GetComponent<Animator>().Play("AnimationVent",5,0);
             //transform.DOMove(new Vector3(camera.transform.position.x - 10,transform.position.y+5,transform.position.z), 3);
         
-            yield return new WaitForSeconds(3f); // de la durée de l'animation
+            yield return new WaitForSeconds(1.5f); // de la durée de l'animation
             GetComponent<Material>().DOFade(0,0.5f);
 
             GetComponent<Material>().DOFade(255,0.5f);
