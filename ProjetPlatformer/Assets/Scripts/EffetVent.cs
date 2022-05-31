56fb8bc39cf46edcf207c3228fdd046fa41d3a3c
Elythenes <e.dehay@rubika-edu.com>
2022-05-31 18:22:24 +0200

    tentative amélioration tempete

--- ProjetPlatformer/Assets/Scripts/EffetVent.cs
+++ ProjetPlatformer/Assets/Scripts/EffetVent.cs
@@ -11,6 +11,7 @@ public class EffetVent : MonoBehaviour
     public float yPos;
 
     public Animator animTempete;
+    public Animator TempeteManager;
 
     public static EffetVent instance;
     public WindArea windArea;
@@ -33,7 +34,7 @@ public class EffetVent : MonoBehaviour
     public void Update()
     {
         //StartCoroutine(AttenteVent());
-        transform.position = camera.transform.position - new Vector3(xPos, yPos, camera.transform.position.z);
+       // transform.position = camera.transform.position - new Vector3(xPos, yPos, camera.transform.position.z);
         
 
         if (windArea.isWindy == false)
@@ -42,12 +43,16 @@ public class EffetVent : MonoBehaviour
             //GetComponent<Animator>().Play("AnimationVent",5);
             animTempete.SetBool("CanBegin", true);
             animTempete.SetBool("CanEnd", false);
+            TempeteManager.SetBool("Start",true);
+            TempeteManager.SetBool("End",false);
         }
         else 
         {
             renderer.enabled = false;
             animTempete.SetBool("CanEnd", true);
             animTempete.SetBool("CanBegin", false);
+            TempeteManager.SetBool("Start",false);
+            TempeteManager.SetBool("End",true);
         }
         
     }
@@ -58,7 +63,7 @@ public class EffetVent : MonoBehaviour
         if (WindArea.instance.letsHaveTempete)
         {
             GetComponent<Animator>().Play("AnimationVent",5,0);
-            //transform.DOMove(new Vector3(camera.transform.position.x - 10,transform.position.y+5,transform.position.z), 3);
+            transform.DOMove(new Vector3(camera.transform.position.x - 10,transform.position.y+5,transform.position.z), 3);
         
             yield return new WaitForSeconds(1.5f); // de la durée de l'animation
             GetComponent<Material>().DOFade(0,0.5f);
